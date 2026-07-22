1a01cc4ff0a44ba250f3350fa924d14fb470e866
Nitish Sharma <ernitishsharma361@gmail.com>
2026-07-22 23:12:38 +0530

    login and register done

---
+++ backend/src/controller/auth.controller.js
@@ -0,0 +1,106 @@
+const userModel = require("../model/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+async function registerUser(req, res) {
+  const { username, email, password } = req.body;
+
+  try {
+    // Check if the user already exists
+    const existingUser = await userModel.findOne({ $or: [
+      { username }, { email }
+    ] });
+    
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const user = new userModel({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    
+    // Generate a token
+    const token = jwt.sign({
+       id: user._id,
+       username: user.username,
+      }, process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    // Save the user to the database
+    await user.save();
+
+    // Set the token in the cookie
+    res.cookie("token", token, { httpOnly: true });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+      token
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+async function loginUser(req, res) {
+  const { email, password, username } = req.body;
+
+  try {
+    // Check if the user exists
+    const user = await userModel.findOne({ $or: [
+      { username }, { email }
+    ] });
+
+    if (!user) {
+      return res.status(401).json({ message: "invalid credentials" });
+    }
+
+    // Compare the password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials password" });
+    }
+
+    // Generate a token
+    const token = jwt.sign({
+       id: user._id,
+       username: user.username,
+      }, process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    // Set the token in the cookie
+    res.cookie("token", token, { httpOnly: true });
+
+    res.status(200).json({
+      message: "User logged in successfully",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+      token
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+module.exports = {
+  registerUser,
+  loginUser
+};
